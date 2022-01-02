7975b5986a9907c2f43898f3b8274763df0c872e
Sadia Tasnim <56369850+sleepycatSadia@users.noreply.github.com>
2022-01-03 03:31:26 +0600

    Update LabA5_Task02.asm

--- Lab Tasks/LabA5_Task02.asm
+++ Lab Tasks/LabA5_Task02.asm
@@ -15,13 +15,20 @@
     inputPrompt3 DB "Example: Use F instead of f to get correct output. Thank you. $"
     inputPrompt4 DB "Append a 0 before if you insert single digit.eg write 04 instead of 4. $"  
     numPrompt DB "Enter number in 2 HEX bits= $"             
-    outputPromptDec DB  "Sum of ODD Numbers in DECIMAL : $" 
-    outputPromptHex DB  "Sum of ODD Numbers in HEXADECIMAL : $"             
+    outputPromptDec DB  "Sum of ODD Numbers in DECIMAL : $"          
     one DB 01
     base DB 16  
-    divisor256 DW 256
+    
+    divisor10    DB 10 
+    divisor10W   DW 10 
+        
+    bit4 DB ?
+    bit3 DB ?
+    bit2 DB ?
+    
+
     divisor2 DB 2  
-    divisor10 DB 10     
+
     sum  DB ?
 .CODE
     MAIN PROC
@@ -233,159 +240,253 @@
         
         
     
-        LEA DX,outputPromptHex   ;output statement
-        MOV AH,9                 ;string output
-        INT 21H                  ;interrupt
-        
-        Print3HexBits:           ;now 3 HEX bits are in BX
-        MOV AX,00                ;resetting AX
-        MOV CX,BX                ;storing sum in CX to use 
-                                 ;it when printing dec version
-        MOV AX,BX                ;moving 3 HEX bits in AX
-        CMP AX,255               ;to check if the sum has 3 HEX bits 
-        JG ThirdBitNot0          ;if sum>=255D it has non zero third bit 
-        MOV DL,00                ;if sum<255D third bit of sum=0
-        JMP ThirdBitIsInDLnow:   ;required third bit stored in DL
-        
-        ThirdBitNot0:            ;calculate to get nonzero 3rd bit
-        DIV divisor256           ;hex num /256 to get quotient
-        
-        MOV DL,AL                ;DL=quotient=non zero 3rd bit
-        
-        ThirdBitIsInDLnow:
-        CMP DL ,divisor10        ;check if bit is in range A-F
-        JGE GetAscii3            ;if bit >=10h fix ascii accordingly
-        ADD DL,30H               ;if bit is range 1h-9h
-        Ascii3Done:              ;ascii fixed
-        MOV AH,2                 ;printing bit
-        INT 21H 
-        
-        MOV AX,00                ;resetting AX
-        MOV AL,CL                ;now print remaining 2 bits of HEX sum
-        DIV base                 ;hex num /16 to get remainder
-    
-        MOV DL,AL                ;storing the quotient in DL
-        MOV DH,AH                ;storing remainder in DH
-        
-        CMP DL ,divisor10        ;check if bit is in range A-F
-        JGE GetAscii1            ;if bit >=10h fix ascii accordingly
-        ADD DL,30H               ;if bit is range 1h-9h
-        Ascii1Done:              ;ascii fixed
-        MOV AH,2                 ;printing bit
-        INT 21H                  ;interrupt
-        
-        MOV DL,DH                ;moving the remainder in DL to print
-        
-        CMP DL ,divisor10        ;check if bit is in range A-F
-        JGE GetAscii2            ;if bit >=10h fix ascii accordingly
-        ADD DL,30H               ;if bit is range 1h-9h
-        Ascii2Done:              ;ascii fixed
-        MOV AH,2                 ;printing bit
-        INT 21H                  ;interrupt
-        
-        MOV AH, 2                ;set cursor position
-        MOV DL, 0DH              ;carriage return
-        INT 21H                  ;interrupt             
-        MOV DL, 0AH              ;line feed
-        INT 21H                  ;interrupt
-              
-        JMP  PrintDec            ;3 HEX bits printing completed
-            
-            
-            
-        GetAscii1:
-        ADD DL,37H               ;example:HEX A >> B+37=HEX 41=ascii A
-        JMP Ascii1Done:      
-        
-        GetAscii2:
-        ADD DL,37H               ;example:HEX C >> C+37=HEX 43=ascii C
-        JMP Ascii2Done:
-            
-        GetAscii3:
-        ADD DL,37H               ;example:HEX F >> F+37=HEX 46=ascii F
-        JMP Ascii3Done:
-                
-        PrintDec:
-        
         LEA DX,outputPromptDec   ;output statement
         MOV AH,9                 ;string output
         INT 21H                  ;interrupt
+                
+        PrintDec:
         
-        PrintSum:                ;sum is stored in CX
-        MOV DX,CX                ;DX=sum
-        
-        MOV AX,DX                ;AX=4 DEC bits of OddSum
-        DIV divisor10            ;AX/10
-        MOV DX,AX                ;storing the rem and quotient
-        MOV AL,00                ;we just need the rem so reset AH
-        PUSH AX                  ;storing the 1st DEC bit
-        
-        MOV DH,00                ;reset DH
-        
-        MOV AX,DX                ;AX=3 DEC bits of OddSum
-        DIV divisor10            ;AX/10
-        MOV DX,AX                ;storing the rem and quotient
-        MOV AL,00                ;we just need the rem so reset AH
-        PUSH AX                  ;storing the 2nd DEC bit
-        
-        MOV DH,00                ;reset DH
-        
-        MOV AX,DX                ;AX=2 DEC bits of OddSum
-        DIV divisor10            ;AX/10
-        MOV DX,AX                ;storing the rem and quotient
-        MOV AL,00                ;we just need the rem so reset AH
-        PUSH AX                  ;storing the 3rd DEC bit
-        
-        MOV DH,00                ;reset DH
-        
-        MOV AX,DX                ;AX=1 DEC bit  of OddSum
-        DIV divisor10            ;AX/10
-        MOV DX,AX                ;storing the rem and quotient
-        MOV AL,00                ;we just need the rem so reset AH
-        PUSH AX                  ;storing the 4th DEC bit
-        
-        POP AX                   ;popping 4th DEC bit
-        MOV DL,AH                ;DL=4th bit
-        ADD DL,30H               ;getting proper Ascii value
-        MOV AH,2                 ;printing char
-        INT 21H                  ;interrupt
-        
-        POP AX                   ;popping 3rd DEC bit
-        MOV DL,AH                ;DL=3rd bit
-        ADD DL,30H               ;getting proper Ascii value
-        MOV AH,2                 ;printing char
-        INT 21H                  ;interrupt
-        
-        POP AX                   ;popping 2nd DEC bit
-        MOV DL,AH                ;DL=2nd bit
-        ADD DL,30H               ;getting proper Ascii value
-        MOV AH,2                 ;printing char
-        INT 21H                  ;interrupt
-        
-        POP AX                   ;popping 1st DEC bit
-        MOV DL,AH                ;DL=1st bit
-        ADD DL,30H               ;getting proper Ascii value
-        MOV AH,2                 ;printing char
-        INT 21H                  ;interrupt
-       
-        
-        JMP EXIT 
+    ;--------------------------------------
+    ;Printing the sum bit by bit     
+    ;We considered the sum is in 4 dec bits
+    ;First we will split all 4 bits
+    ;then we print them
+    ;--------------------------------------
+    
+    ;SUM is in BX right now
+    
+    MOV AX,00                ;reset AX
+    MOV DX,00                ;reset DX 
+    
+    ;--------------
+    ;splitting bits
+    ;--------------
+    
+    PrintSum:                ;sum is stored in CX
+           
+    ;---------------------------------------------------
+    ;We will extract the 1st bit 
+    ;by manually dividing the sum by 10
+    ;and then get the remainder 
+    ;the remainder will be the 1st bit
+    ;We used manual division because
+    ;if the sum is big,then after dividing by 10,
+    ;the quotient we will get will be more tha FF.   
+    ;example : 2567/10 Q=256>FF(255d)  
+    ;
+    ;If the quotient is more than FF, then it
+    ;can't be stored in AL(default location for quotient) 
+    ;because AL=8 bit register so max value that can be
+    ;stored here without causing any overflow is 255d /FF
+    ;
+    ;So while manually dividing we will store the 
+    ;quotient  in BX -will be used to extract next(2nd) bit
+    ;remainder in AX -will be pushed in STACK     
+    ;
+    ;While doing repetitive subtraction
+    ;the subtraction result will be in AX
+    ;---------------------------------------------------
+    
+    MOV DX,BX                ;DX=sum
+    MOV BX,00H               ;BX=subCount=0
+                             ;it keeps track how many times we 
+                             ;could subtract 10 from the sum
+                             ;BX=quotient will be stored here
+    
+    MOV AX,DX                ;AX=4 DEC bits of Sum
+    
+    LoopFor1stBit:           ;loop for extracting 1st bit
+    
+    CMP AX,divisor10W        ;compare current subtraction result with 10
+    JL breakLoop             ;if current sub result < 10,
+                             ;then break the repetitive subtraction  
+                             
+                             ;else sub result>=10,we can subtract again
+    SUB AX,divisor10W        ;sum=sum-10
+    INC BX                   ;subCount++
+    
+    JMP LoopFor1stBit        ;continue subtracting loop
+    breakLoop:               ;manual division complete 
+    MOV AH,AL                ;the rem is in AL,we move it to AH
+    MOV AL,00                ;reset AL 
+    PUSH AX                  ;AX=remainder=1st bit=pushed to stack
+    MOV DX,BX                ;BX=quotient
+    
+    ;----------------------------------------                         
+    ;DX has the quotient found from prev step  
+    ;dividing this quotient by 10
+    ;remainder=3rd bit=will be pushed to stack 
+    ;----------------------------------------
+    
+    MOV AX,DX                ;AX=3 DEC bits of Sum
+    DIV divisor10            ;AX/10
+    MOV DX,AX                ;storing the rem and quotient
+    MOV AL,00                ;we just need the rem so reset AH
+    PUSH AX                  ;storing the 2nd DEC bit
+    MOV DH,00                ;reset DH                         
+    ;----------------------------------------                         
+    ;DX has the quotient found from prev step  
+    ;dividing this quotient by 10
+    ;remainder=2nd bit=will be pushed to stack 
+    ;----------------------------------------
+    
+    MOV AX,DX                ;AX=2 DEC bits of Sum
+    DIV divisor10            ;AX/10
+    MOV DX,AX                ;storing the rem and quotient
+    MOV AL,00                ;we just need the rem so reset AH
+    PUSH AX                  ;storing the 3rd DEC bit
+    MOV DH,00                ;reset DH               
+    
+    ;----------------------------------------                         
+    ;DX has the quotient found from prev step  
+    ;dividing this quotient by 10
+    ;remainder=1st bit=will be pushed to stack 
+    ;----------------------------------------
+    
+    MOV AX,DX                ;AX=1 DEC bit  of Sum
+    DIV divisor10            ;AX/10
+    MOV DX,AX                ;storing the rem and quotient      
+    MOV AL,00                ;we just need the rem so reset AH
+    PUSH AX                  ;storing the 4th DEC bit
+               
+     
+    
+    ;--------------
+    ;printing bits
+    ;--------------
+    
+     
+               
+    ;Here 4th bit=MSB bit, 1st bit=LSB bit
+    ;3rd & 2nd bits are bits in between 1st & 4th
+    ;If the Total Decimal Sum=3125 ,then
+    ;bit4=3, bit3=1, bit2=2, bit1=5
+               
+    ;--------------------------
+    ;Skipping/Printing 4th bit  
+    ;if bit4=0 we don't print it
+    ;else we print it
+    ;--------------------------           
+    
+    POP AX                   ;popping 4th DEC bit
+    MOV DL,AH                ;DL=4th bit
+    MOV bit4,DL              ;storing 4th bit in bit4 var
+    CMP bit4,00              ;check if 4th bit is 0
+    JE SkipPrintBit4         ;if bit4 is 0,no need to print it
+                             ;if bit4 not 0,we print it    
+                             
+    ADD DL,30H               ;getting proper Ascii value    
+    MOV AH,2                 ;printing char
+    INT 21H                  ;interrupt
+    
+    SkipPrintBit4:           ;move on with/without printing bit4
+    
+    ;-------------------------------------------
+    ;Skipping/Printing 3rd bit       
+    ;if bit3,bit4 both =0,we don't print bit3 
+    ;else we print it   
+    ;-------------------------------------------
+    
+    
+    POP AX                   ;popping 3rd DEC bit
+    MOV DL,AH                ;DL=3rd bit
+    MOV bit3,DL              ;storing 3rd bit in bit3 var 
+    CMP bit3,00              ;check if 3rd bit is 0
+    
+    JE CheckBit4is0forPrintingBit3 ;if bit3 is 0  ,
+                                   ;We check if bit4=0 too/not 
+                                         
+    JG PrintBit3             ;if bit3=0,no need to check bit4
+                             ;we print bit3
+    
+    CheckBit4is0forPrintingBit3:
+    CMP bit4,00              ;compare bit 4 with 0
+    JE SkipPrintBit3         ;if bit4=0 given that bit3=0
+                             ;we skip printing bit3
+                             
+                             ;if bit3=0 but bit4 not 0
+                             ;we print the bit3
+                             
+    PrintBit3:               ;prints bit3
+                             
+                              
+    ADD DL,30H               ;getting proper Ascii value
+    MOV AH,2                 ;printing char
+    INT 21H                  ;interrupt
+    
+    
+    SkipPrintBit3:           ;move on  with/without printing bit3
+    
+                
+    ;----------------------------------------
+    ;Skipping/Printing 2nd bit  
+    ;if bit2=bit3=bit4=0 we don't print bit2
+    ;else we print it
+    ;---------------------------------------- 
+    
+    POP AX                   ;popping 2nd DEC bit
+    MOV DL,AH                ;DL=2nd bit
+    MOV bit2,DL              ;storing 2nd bit in bit2 var
+    CMP bit2,00              ;check if 2nd bit is 0
+    
+    JE CheckBit3is0forPrintingBit2 ;if bit2 is 0    
+                                   ;We check if bit3=0 too/not    
+    
+    JG PrintBit2             ;if bit2 not 0
+                             ;we print bit2
+                              
+   
+    CheckBit3is0forPrintingBit2:
+    CMP bit3,00                    ;compare bit3 with 0
+    JE CheckBit4is0forPrintingBit2:;if bit3=0 
+                                   ;we need to check if bit4=0 too/not
+   
+    JG PrintBit2             ;if bit3 not 0 even though bit2= 0
+                             ;we need to print bit2
+    
+    CheckBit4is0forPrintingBit2:
+    CMP bit4,00              ;compare bit4 with 0
+    JE SkipPrintBit2         ;if bit4=0 too ,then bit4=bit3=bit2=0
+                             ;we skip printing bit2
+                             
+                             ;if bit2=bit3=0 but bit4 not 0
+                             ;we print the 2nd bit
+                             
+    PrintBit2:               ;prints bit2
+                             
+    ADD DL,30H               ;getting proper Ascii value
+    MOV AH,2                 ;printing char
+    INT 21H                  ;interrupt
+    
+    SkipPrintBit2:           ;move on  with/without printing bit2
+    
+    ;--------------------------
+    ;Printing 1st bit
+    ;-------------------------- 
+    
+    
+    POP AX                   ;popping 1st DEC bit
+    MOV DL,AH                ;DL=1st bit
+    ADD DL,30H               ;getting proper Ascii value
+    MOV AH,2                 ;printing char
+    INT 21H                  ;interrupt
+   
+    JMP EXIT 
         
         
         
         AddNumToSum: 
         
         ADD BX,DX                ;add ODD num in sum
-        JMP DoneAdding
-        
-                   
-        EXIT:    
-        
-
-        ; YOUR CODE ENDS HERE
-        
-        MOV AX, 4C00H
-        INT 21H
-        
-    MAIN ENDP
-    END MAIN
+        JMP DoneAdding  
+    
+    EXIT:    
+    ; YOUR CODE ENDS HERE
+    
+    MOV AX, 4C00H
+    INT 21H
+    
+    MAIN ENDP    5
+    END MAIN 
+message.txt
+21 KB
